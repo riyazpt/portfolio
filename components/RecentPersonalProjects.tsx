a078f389b392f9e0ee901302fa082bf26a5c4cc2
Riyas <riyazpt@gmail.com>
2024-08-28 19:32:57 +0400

    working portfolio

---
+++ components/RecentPersonalProjects.tsx
@@ -0,0 +1,86 @@
+import {projects} from "@/data";
+import React from "react";
+
+import {FaLocationArrow} from "react-icons/fa6";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+const PinContainer = dynamic(
+  () => import("@/components/ui/PinContainer").then((mod) => mod.PinContainer),
+  {ssr: false}
+);
+function RecentPersonalProjects() {
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+        Personal {""}
+        <span className="text-purple">Projects</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+        {projects.map((item) => (
+          <>
+            {item.id > 4 ? (
+              <div
+                key={item.id}
+                className="sm:h[41rem] h[32rem] lg:min-h-[32.5rem]  flex items-center justify-center sm:w-[570px] w-[80vw]"
+              >
+                <PinContainer title={item.title} href={item.link}>
+                  <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] lg:h-[30vh] mb-10">
+                    <Image
+                      src={item.img}
+                      alt="cover"
+                      layout="fill"
+                      className="z-10 absolute bottom-0"
+                      objectFit="cover"
+                    />
+                  </div>
+
+                  <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                    {item.title}
+                  </h1>
+
+                  <p
+                    className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                    style={{color: "#BEC1DD", margin: "1vh 0"}}
+                  >
+                    {item.des}
+                  </p>
+
+                  <div className="flex items-center justify-between mt-7 mb-3">
+                    <div className="flex items-center">
+                      {item.id != 4 &&
+                        item.iconLists.map((icon, index) => (
+                          <div
+                            key={`icon-${index}`}
+                            className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                            style={{
+                              transform: `translateX(-${5 * index + 2}px)`,
+                            }}
+                          >
+                            <img src={icon} alt="icon5" className="p-2" />
+                          </div>
+                        ))}
+                    </div>
+                    {item.id != 4 ? (
+                      <div className="flex justify-center items-center">
+                        <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                          Check Live Site
+                        </p>
+                        <FaLocationArrow className="ms-3" color="#CBACF9" />
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </PinContainer>
+              </div>
+            ) : (
+              ""
+            )}
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default RecentPersonalProjects;
---
+++ data/index.ts
@@ -0,0 +1,190 @@
+export const navItems = [
+  {name: "About", link: "#about"},
+  {name: "Projects", link: "#projects"},
+  {name: "Contact", link: "#contact"},
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "The Treasury savings bank",
+    category: "Professional Projects",
+    des: "The Treasury savings bank of Kerala is a unique system in India. It provides facilities for Savings Bank (SB) operations and Fixed Deposits",
+    img: "/project-tsb.png",
+    iconLists: [],
+    link: "https://tsbonline.kerala.gov.in/",
+  },
+  {
+    id: 2,
+    title: "Q-Portal",
+    category: "Professional Projects",
+    des: "Experience effortless venue management with our all-in-one software. Simplify scheduling, ticketing, customer profiles, advanced analytics, sales reports, and marketing",
+    img: "/project-q-portal.png",
+    iconLists: [],
+    link: "https://www.qportal.io/",
+  },
+  {
+    id: 3,
+    title: "Tax-Returns-PLUS",
+    category: "Professional Projects",
+    des: "Portal for Filing your Tax Returns for UK&Ireland contractors",
+    img: "/project-tax-returns.png",
+    iconLists: [],
+    link: "https://www.taxreturnsplus.com/userLogin",
+  },
+  {
+    id: 4,
+    title: "",
+    des: "",
+    img: "",
+    iconLists: [""],
+    link: "",
+  },
+  {
+    id: 5,
+    title: "Real Estate System",
+    des: "This project is demo of Real Estate App which lists properties for sale properties for rent and filter according to specifications Using NextJs",
+    img: "/project-real-estate.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg"],
+    link: "https://real-estate-sp3w.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Meet Up",
+    des: "MeetUp app Using nextjs and React Js and mongodb",
+    img: "/project-meetup.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg"],
+    link: "https://meet-up-eta.vercel.app/",
+  },
+  {
+    id: 7,
+    title: "Hoo-bank Website",
+    des: "hoo-bank landing page Using  nextjs and React Js",
+    img: "/project-hoo-bank.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg"],
+    link: "https://hoo-bank-rose-nine.vercel.app/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Senior Software Engineer - Mideast Data Systems, UAE",
+    desc: "Engineered a high-speed single-page web app using React.js, Laravel, and NextJS, significantly cutting page load times.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Senior Software Engineer - IPStudios, UAE",
+    desc: "Revitalized an existing web project by migrating it from CMS to Laravel and React JS, achieving a 30% reduction in page load time.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Senior Software Developer - Sacoor Brothers, UAE",
+    desc: "Collaborated on the development of the Stock Replenishment Module, fostering teamwork and providing peer guidance to enhance overall project efficiency.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Senior Software Engineer - Krews Ltd, Abu Dhabi, UAE",
+    desc: "Helped the organization to build better architecture for B2B and B2C web projects using Laravel, which includes database design and performance tuning.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 5,
+    title: "Senior Software Engineer - Contracting Plus India Pvt Ltd, India",
+    desc: "Designed and developed new features that improved user experience and increased company revenue by attracting 30% more clients to file tax returns through the company portal.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 6,
+    title: "Technical Lead - National Informatics Center (NIC), India",
+    desc: "Managed a team of developers and was involved in task delegation, and peer guidance, and converted business logic to stored procedures and functions using DB2.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/riyazpt/",
+  },
+
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/riyazpt/",
+  },
+];

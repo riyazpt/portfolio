6794d909f72e1896a9995f355a7e50fc9565fad5
Riyas <riyazpt@gmail.com>
2024-09-02 17:43:28 +0400

    Phone number corrected

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -7,7 +7,7 @@ function ContactSection() {
       <h2 className="text-center text-2xl font-bold mb-4">Contact Me</h2>
       <div className="flex items-center justify-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
         <FaMobileAlt className="mr-2" />
-        <span>+971504320046</span>
+        <span>+971504323046</span>
       </div>
       {/* Additional contact details or sections can go here */}
     </div>
